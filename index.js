78854eede6c2ee2c3049c9ddd3dc827de78a14cb
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2019-12-17 23:06:14 -0300

    index update

---
+++ index.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+axios
+    .get('https://motivaai.nandomoreira.me/')
+    .then(response => {
+        if (response.status === 200) {
+            const $ = cheerio.load(response.data);
+            const data = {
+                phrase: $('p').first().text(),
+                author: $('small').first().text()
+            };
+            console.log(data);
+        } else {
+            console.log('err');
+        }
+    })
+    .catch(console.log);
