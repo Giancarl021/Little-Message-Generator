0d94c8de0d268913bff0704152d3e4015e0d4db2
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2019-12-22 23:37:50 -0300

    Video bot initalized

--- bots/video.js
+++ bots/video.js
@@ -1,5 +1,31 @@
+const videoshow = require('videoshow');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffprobePath = require('@ffprobe-installer/ffprobe').path;
+ffmpeg.setFfmpegPath(ffmpegPath);
+ffmpeg.setFfprobePath(ffprobePath);
+
 async function main(data) {
-    console.log(data);
+    const options = {
+        fps: 25,
+        loop: 5, // seconds
+        transition: true,
+        transitionDuration: 1, // seconds
+        videoBitrate: 8000,
+        size: '1920x1080',
+        audioBitrate: '128k',
+        audioChannels: 2,
+        format: 'mp4',
+        videoCodec: 'libx264',
+        pixelFormat: 'yuv420p'
+    };
+    return new Promise((resolve, reject) => {
+        videoshow(data.image, options)
+            .audio(data.music.path)
+            .save('out.mp4')
+            .on('error', reject)
+            .on('end', resolve)
+    });
 }
 
 module.exports = main;
